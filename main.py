c91c0d354404e25abff51bdbb41bfce2cb8d7540
Ivan_Andrianov <andreyanovi@yandex.ru>
2024-11-14 13:18:58 +0300

    plotting and setting the appearance settings:

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ warnings.filterwarnings('ignore')
 # устанавливаем параметры изображения
 # показываем все столбцы
 pd.set_option('display.max_columns', None)
-# задаём ширину таблицы в 1 000 пикселей
+# задаём ширину таблицу в 1 000 пикселей
 pd.set_option('display.width', 1000)
 # ограничиваем вывод первыми 15 строками
 pd.set_option('display.max_rows', 10)
@@ -23,40 +23,39 @@ df = pd.read_csv(file_path)
 
 # задаём ключевые слова для поиска фильмов Marvel
 marvel_keywords = [
-    "Avengers", "Black Panther", "Captain America", "Doctor Strange", "Eternals",
-    "Falcon", "Guardians of the Galaxy", "Hawkeye", "Hulk", "Iron Man", "Loki",
-    "Scarlet Witch", "Shang-Chi", "Spider-Man", "Thor", "WandaVision", "Ant-Man",
-    "Black Widow", "Captain Marvel", "Deadpool", "X-Men", "Wolverine", "Fantastic Four",
-    "Ms. Marvel", "Moon Knight", "She-Hulk", "Daredevil", "Punisher", "Jessica Jones",
-    "Luke Cage", "Iron Fist", "Inhumans", "What If...?", "Mutant X", "Secret Invasion", "Blade",
-    "Agents of S.H.I.E.L.D.",
-    "Fantastic 4"
+   "Avengers", "Black Panther", "Captain America", "Doctor Strange", "Eternals",
+   "Falcon", "Guardians of the Galaxy", "Hawkeye", "Hulk", "Iron Man", "Loki",
+   "Scarlet Witch", "Shang-Chi", "Spider-Man", "Thor", "WandaVision", "Ant-Man",
+   "Black Widow", "Captain Marvel", "Deadpool", "X-Men", "Wolverine", "Fantastic Four",
+   "Ms. Marvel", "Moon Knight", "She-Hulk", "Daredevil", "Punisher", "Jessica Jones",
+   "Luke Cage", "Iron Fist", "Inhumans", "What If...?", "Mutant X", "Secret Invasion", "Blade", "Agents of S.H.I.E.L.D.",
+   "Fantastic 4"
 ]
 
 # задаём ключевые слова для поиска фильмов DC
 dc_keywords = [
-    "Batman", "Superman", "Wonder Woman", "Aquaman", "Flash", "Green Lantern",
-    "Joker", "Shazam", "Justice League", "Suicide Squad", "Harley Quinn", "Batwoman",
-    "Arrow", "Supergirl", "Doom Patrol", "Titans", "Black Adam", "Peacemaker",
-    "Constantine", "Swamp Thing", "Watchmen", "Green Arrow", "Blue Beetle",
-    "Hawkman", "Zatanna", "Catwoman", "Cyborg", "Teen Titans", "Darkseid", "Smallvile"
+   "Batman", "Superman", "Wonder Woman", "Aquaman", "Flash", "Green Lantern",
+   "Joker", "Shazam", "Justice League", "Suicide Squad", "Harley Quinn", "Batwoman",
+   "Arrow", "Supergirl", "Doom Patrol", "Titans", "Black Adam", "Peacemaker",
+   "Constantine", "Swamp Thing", "Watchmen", "Green Arrow", "Blue Beetle",
+   "Hawkman", "Zatanna", "Catwoman", "Cyborg", "Teen Titans", "Darkseid", "Smallvile"
 ]
 
 
 # создаём функцию классификации
 def classify_movie(title):
-    # проверяем, есть ли в заголовке ключевые слова фильмов Marvel
-    for keyword in marvel_keywords:
-        # если такие есть, маркируем фильм как Marvel
-        if keyword in title:
-            return 'Marvel'
-    # проверяем, есть ли в заголовке ключевые слова фильмов DC
-    for keyword in dc_keywords:
-        # если такие есть, маркируем фильм как DC
-        if keyword in title:
-            return 'DC'
-    # если в названии нет таких слов, возвращаем значение Unknown
-    return 'Unknown'
+   # проверяем, есть ли в заголовке ключевые слова фильмов Marvel
+   for keyword in marvel_keywords:
+       # если такие есть, маркируем фильм как Marvel
+       if keyword in title:
+           return 'Marvel'
+   # проверяем, есть ли в заголовке ключевые слова фильмов DC
+   for keyword in dc_keywords:
+       # если такие есть, маркируем фильм как DC
+       if keyword in title:
+           return 'DC'
+   # если в названии нет таких слов, возвращаем значение Unknown
+   return 'Unknown'
 
 
 # применяем функцию к столбцу с названием Movie
@@ -75,7 +74,7 @@ unknown_franchise = df[df['Franchise'] == 'Unknown']
 print('\nВыводим датасет с фильмами без франшиз')
 print(unknown_franchise.head())
 
-# фильтруем данные: оставляем только те строки, где рейтинг IMDb не равен 0
+# фильтруем данные: удаляем все, где рейтинг IMDb = 0
 df = df[df['IMDB_Score'] != 0]
 # фильтруем данные: удаляем все, где столбец Franchise = Unknown
 df = df[df['Franchise'] != 'Unknown']
@@ -90,7 +89,6 @@ df_unique = df.drop_duplicates(subset='Movie')
 # используя метод value_counts(), подсчитываем
 # количество уникальных фильмов для обеих франшиз
 franchise_counts = df_unique['Franchise'].value_counts()
-
 # указываем в дюймах размер будущего графика
 plt.figure(figsize=(8, 6))
 # строим столбчатую диаграмму kind='bar'
@@ -108,7 +106,6 @@ plt.show()
 # строим график 2: средний рейтинг фильмов
 # группируем фильмы по франшизам и вычисляем средний рейтинг
 average_ratings = df_unique.groupby('Franchise')['IMDB_Score'].mean()
-
 # строим столбчатую диаграмму kind='bar'
 average_ratings.plot(kind='bar', color=['blue', 'red'], figsize=(10, 8))
 # устанавливаем подпись для всего графика
@@ -120,10 +117,9 @@ plt.xticks(rotation=35)
 # отрисовываем график
 plt.show()
 
-# строим график 3: количество фильмов Marvel и DC по годам
+# cтроим график 3: количество фильмов Marvel и DC по годам
 # подсчитываем количество фильмов для каждой комбинации год-франшиза
 movies_per_year = df_unique.groupby(['Year', 'Franchise']).size().unstack().fillna(0)
-
 # строим линейный график: по оси X идут годы, по Y — количество фильмов
 movies_per_year.plot(kind='line', figsize=(10, 8))
 # устанавливаем название графика
@@ -136,10 +132,79 @@ plt.ylabel('Количество фильмов')
 plt.xticks(rotation=35)
 # отрисовываем график
 plt.show()
-<<<<<<< HEAD
-=======
 
-# строим график 4: фильмы с высоким рейтингом IMDb
+
+# cтроим график 4: фильмы с высоким рейтингом IMDb
 # фильтруем данные: оставляем только фильмы с рейтингом от 8.0 и выше
 top_movies = df_unique[df_unique['IMDB_Score'] >= 8.0]
->>>>>>> b0d01fb (initial loading and charting)
+# создаём диаграмму scatter plot с использованием библиотеки Plotly Express
+fig = px.scatter(
+   # используем отфильтрованный фрагмент датасета
+   top_movies,
+   # устанавливаем значение оси X из датасета
+   x='Year',
+   # устанавливаем значение оси Y из датасета
+   y='IMDB_Score',
+   # устанавливаем зависимость цвета точек от франшизы
+   color='Franchise',
+   # при наведении на точку выводим название фильма
+   hover_data=['Movie'],
+   # устанавливаем название графика
+   title='Фильмы Marvel и DC c высоким рейтингом'
+)
+# поворачиваем название меток по X на 270 градусов для удобства
+fig.update_xaxes(tickangle=270)
+# отрисовываем график
+fig.show()
+
+
+# строим график 5: график по жанрам
+# преобразуем значения в столбце Genre в список жанров для каждого фильма
+df_unique['Genres_List'] = df_unique['Genre'].str.split(',')
+# функция explode() создаёт дубликаты фильма для каждого жанра
+df_exploded = df_unique.explode('Genres_List')
+# функция dropna удаляет строки с пустыми жанрами — NaN
+df_exploded_clean = df_exploded.dropna(subset=['Genres_List'])
+# удаляем методом .strip() строки, где на месте жанра стоят пробелы или пустые строки
+df_exploded_clean = df_exploded_clean[df_exploded_clean['Genres_List'].str.strip() != '']
+# создаём диаграмму treemap с использованием библиотеки Plotly Express
+fig = px.treemap(
+   # используем отфильтрованный фрагмент датасета
+   df_exploded_clean,
+   # создаём вложенную структуру: франшиза → жанр → фильм
+   path=['Franchise', 'Genres_List', 'Movie'],
+   # задаём название графика
+   title='Жанры фильмов DC и Marvel',
+   # при наведении на ячейку отображаем рейтинг
+   hover_data=['IMDB_Score'],
+   # цвет ячейки зависит от рейтинга IMDb
+   color='IMDB_Score',
+   # устанавливем цветовую тему Viridis
+   color_continuous_scale='Viridis'
+)
+# отрисовываем график
+fig.show()
+
+
+# строим график 6: IMDb-рейтинг / длительность фильма
+# извлекаем количество минут и переводим значение в тип float
+df_unique['RunTime_Min'] = df_unique['RunTime'].str.extract('(\d+)').astype(float)
+# создаём диаграмму scatter plot с использованием библиотеки Plotly Express
+fig = px.scatter(
+   # используем отфильтрованный фрагмент датасета
+   df_unique,
+   # устанавливаем значение оси X из датасета
+   x='Year',
+   # устанавливаем значение оси Y из датасета
+   y='IMDB_Score',
+   # размер обозначающей фильм точки зависит от продолжительности фильма
+   size='RunTime_Min',
+   # цвет зависит от франшизы
+   color='Franchise',
+   # при наведении отображаем название фильма
+   hover_name='Movie',
+   # устанавливаем заголовок графика
+   title='Длительность и рейтинг IMDb'
+)
+# отрисовываем график
+fig.show()
